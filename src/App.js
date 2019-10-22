85c0d48648a2c84bfef85691a1f5d7ea187a6403
Andrey Castro <andrey.castro@possible.com>
2019-10-22 15:25:41 -0600

    [:lipstick:] Basic layout distribution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Gallery from './components/gallery';
+import './sass/main.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="dashboard">
+        <div className="dashboard_main">
+        <div className="dashboard_main-header">
+            This is the header
+          </div>
+          <div className="dashboard_main-gallery aspect-ratio aspect-ratio-16-9">
+            <Gallery/> 
+          </div>
+        </div>
+        <div className="dashboard_aside uk-grid">
+          
+          <div class="uk-card uk-card-default uk-width-1-2">
+            <div class="uk-card-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+            <div class="uk-card-footer">
+              <span class="uk-button uk-button-text">En Proceso</span>
+            </div>
+          </div>
+
+
+
+          <div class="uk-card uk-card-default uk-width-1-2">
+            <div class="uk-card-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+            <div class="uk-card-footer">
+                <span class="uk-button uk-button-text">Listo para cobro</span>
+            </div>
+          </div>
+          <div class="uk-card uk-card-default uk-width-1-2">
+            <div class="uk-card-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+            <div class="uk-card-footer">
+                <span class="uk-button uk-button-text">En Proceso</span>
+            </div>
+          </div>
+          <div class="uk-card uk-card-default uk-width-1-2">
+            <div class="uk-card-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+            <div class="uk-card-footer">
+                <span class="uk-button uk-button-text">En Proceso</span>
+            </div>
+          </div>
+          <div class="uk-card uk-card-default uk-width-1-2">
+            <div class="uk-card-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+            <div class="uk-card-footer">
+                <span class="uk-button uk-button-text">Listo para cobro</span>
+            </div>
+          </div>
+          <div class="uk-card uk-card-default uk-width-1-2">
+            <div class="uk-card-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+            <div class="uk-card-footer">
+                <span class="uk-button uk-button-text">En Proceso</span>
+            </div>
+          </div>
+          <div class="uk-card uk-card-default uk-width-1-2">
+            <div class="uk-card-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+            <div class="uk-card-footer">
+                <span class="uk-button uk-button-text">Listo para cobro</span>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      
     </div>
   );
 }
